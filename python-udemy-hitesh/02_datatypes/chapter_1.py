03e9f6d81cb4501f17f0b736465376bd9e8f452d
Subhajit Tewary <subhajittewary800@outlook.com>
2026-03-07 13:40:13 +0530

    Refactor ID print statements for sugar_amount to improve clarity

--- python-udemy-hitesh/02_datatypes/chapter_1.py
+++ python-udemy-hitesh/02_datatypes/chapter_1.py
@@ -1,8 +1,8 @@
 sugar_amount = 2
 print(f"Initial sugar: {sugar_amount}")
+print(f"ID of 2: {id(sugar_amount)}")
 
 sugar_amount = 12
 print(f"Second Initial sugar: {sugar_amount}")
-
-print(f"ID of 2: {id(2)}")
-print(f"ID of 12: {id(12)}")
+# The ID of 12 will be different from the ID of 2 because integers are immutable in Python. When we assign a new value to sugar_amount, it creates a new integer object in memory, and sugar_amount now references this new object. Therefore, the ID of 12 will be different from the ID of 2.
+print(f"ID of 12: {id(sugar_amount)}")
