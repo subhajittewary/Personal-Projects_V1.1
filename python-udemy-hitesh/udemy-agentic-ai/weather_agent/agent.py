d23b3fde16a7a72d4810f80ed3122f13db9b19ef
Subhajit Tewary <subhajittewary800@outlook.com>
2026-07-01 16:01:20 +0530

    Update environment variables to use GEMINI_API_KEY and configure API key loading in agent.py

--- python-udemy-hitesh/udemy-agentic-ai/weather_agent/agent.py
+++ python-udemy-hitesh/udemy-agentic-ai/weather_agent/agent.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import requests
 import json
+import os
 
 load_dotenv()
 
 genai.configure(
-    api_key="AQ.Ab8RN6I1WeNezFsLpZyeXuFCIo0yOIf1HgfiYQh2p0X8i-GpfQ")
+    api_key=os.getenv("GEMINI_API_KEY"))
 client = genai.GenerativeModel("gemini-2.5-flash")
 
 
