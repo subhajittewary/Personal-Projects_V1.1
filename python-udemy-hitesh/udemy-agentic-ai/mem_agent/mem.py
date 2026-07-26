0986e465624feb391b4535cafd352e92c9d29962
Subhajit Tewary <subhajittewary800@outlook.com>
2026-07-27 00:19:25 +0530

    feat: refactor OpenAI API key handling and improve memory management flow

--- python-udemy-hitesh/udemy-agentic-ai/mem_agent/mem.py
+++ python-udemy-hitesh/udemy-agentic-ai/mem_agent/mem.py
@@ -1,21 +1,29 @@
+from dotenv import load_dotenv
 from mem0 import Memory
 import os
+import json
+from openai import OpenAI
 
-OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+load_dotenv()
+client = OpenAI()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY is missing. Add it to your .env file.")
 
 config = {
     "version": "v1.1",
     "embedder": {
         "provider": "openai",
         "config": {
-            "api_key": OPEN_AI_API_KEY,
+            "api_key": OPENAI_API_KEY,
             "model": "text-embedding-3-small"
         }
     },
     "llm": {
         "provider": "openai",
         "config": {
-                    "api_key": OPEN_AI_API_KEY,
+                    "api_key": OPENAI_API_KEY,
                     "model": "gpt-4.1"
         }
     },
@@ -29,3 +37,41 @@ config = {
 }
 
 mem_client = Memory.from_config(config)
+
+while True:
+    user_query = input("> ")
+    search_memory = mem_client.search(
+        query=user_query, filters={"user_id": "subhajit"})
+
+    memories = [
+        f"ID: {mem.get("id")}\nMemory: {mem.get("memory")}" for mem in search_memory.get("results")
+    ]
+
+    print(f"found memories {memories}")
+
+    SYSTEM_PROMPT = f"""
+    Here is the context about the user:
+    {json.dumps(memories)}
+        """
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": user_query}
+        ]
+    )
+
+    ai_response = response.choices[0].message.content
+
+    print("AI: ", ai_response)
+
+    mem_client.add(
+        user_id="subhajit",
+        messages=[
+            {"role": "user", "content": user_query},
+            {"role": "assistant", "content": ai_response}
+        ]
+    )
+
+    print("memory has been saved...")
